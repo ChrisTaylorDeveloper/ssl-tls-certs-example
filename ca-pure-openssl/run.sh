5f0a3f234a9de482d03be8f8d4fdddfee34f5c71
Chris Taylor <chris@christaylordeveloper.co.uk>
2026-04-17 11:02:40 +0200

    TEMP!

--- ca-pure-openssl/run.sh
+++ ca-pure-openssl/run.sh
@@ -14,15 +14,16 @@ openssl genrsa \
   -out MyCA.key 2048
 
 # INSPECT THE CA PRIVATE KEY
-# openssl rsa -in MyCA.key -noout -text
+# openssl rsa -noout -text \
+#   -in MyCA.key
 
 # CREATE A SELF-SIGNED ROOT CERT FOR THE CA
 # MyCA.crt is installed in a browser.
 # TODO: Maybe I could use -out MyCA.pem ??
-openssl req -x509 -new -nodes -sha256 -days 1826 \
-  -subj '/C=US/ST=SomeState/L=City/O=MyOrg/OU=OrgUnit/CN=MyOrgRootCA' \
-  -key MyCA.key \
-  -out MyCA.crt
+# openssl req -x509 -new -nodes -sha256 -days 1826 \
+#   -subj '/C=US/ST=SomeState/L=City/O=MyOrg/OU=OrgUnit/CN=MyOrgRootCA' \
+#   -key MyCA.key \
+#   -out MyCA.crt
 
 # INSPECT THE ROOT CERT OF THE CA
 # openssl x509 -in MyCA.crt -noout -text
@@ -31,11 +32,11 @@ openssl req -x509 -new -nodes -sha256 -days 1826 \
 # Adds the subjectAltName extension so both base domain and wildcard is covered
 # Modern browsers rely almost exclusively on the SAN field, not CN.
 # # TODO: Add in OU also?
-openssl req -new -newkey rsa:2048 -nodes \
-  -subj "/C=US/ST=State/L=City/O=Organization/CN=example.com" \
-  -addext "subjectAltName = DNS:example.com, DNS:*.example.com" \
-  -keyout server.key \
-  -out server.csr
+# openssl req -new -newkey rsa:2048 -nodes \
+#   -subj "/C=US/ST=State/L=City/O=Organization/CN=example.com" \
+#   -addext "subjectAltName = DNS:example.com, DNS:*.example.com" \
+#   -keyout server.key \
+#   -out server.csr
 
 # INSPECT THE CERT SIGNING REQUEST
-openssl req -noout -text -in server.csr
+# openssl req -noout -text -in server.csr
